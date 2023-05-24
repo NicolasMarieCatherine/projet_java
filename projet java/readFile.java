2a90821c0ed9ff78eb89dfc009f4296d2924b663
Nicolas MC <Ghost111nc@gmail.com>
2023-05-24 22:11:56 +1100

    Question

---
+++ projet java/readFile.java
@@ -0,0 +1,26 @@
+ 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+ 
+
+public class readFile {
+    /*public static void main(String[] args) {
+        readFile("profile.txt");
+    }
+ 
+    public static void readFile(String pathToFile){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(pathToFile)));
+            String ligne;
+            while((ligne = reader.readLine()) != null){
+                if(ligne.startsWith("Nom :")){
+                    String temp[] = ligne.split(":");
+                    System.out.println("Nom :"+temp[1]);
+                }
+            }
+        } catch (Exception ex){
+            System.err.println("Error. "+ex.getMessage());
+        }
+    }*/
+}
