ca0f6509e27b3d6b1361aec2ae41bf23df20bab0
Nicolas MC <Ghost111nc@gmail.com>
2023-05-25 20:12:27 +1100

    .

--- projet java/Question.java
+++ projet java/Question.java
@@ -35,44 +35,37 @@ public class Question {
 
             try {
 
-                
-
-                /* 
-                String question ;
-                String bonne_reponse ="";
-                String mauvais_reponse_1 ="";
-                String mauvais_reponse_2 ="";
-                String mauvais_reponse_3 ="";
-                */
-
                 Scanner sc = new Scanner(System.in);
 
                 System.out.println("Entrer la dificultée  facile / dure / moyenne : ");
                 String dificulte = sc.nextLine();
 
+                System.out.println("\n");
                 System.out.println("Entrer votre question : ");
                 String question = sc.nextLine();
 
+                System.out.println("\n");
                 System.out.println("Entrer la bonne reponse : ");
                 String bonne_reponse =sc.nextLine();
                 
+                System.out.println("\n");
                 System.out.println("Entrer une mauvaise reponse : ");
                 String mauvais_reponse_1 =sc.nextLine();
 
+                System.out.println("\n");
                 System.out.println("Entrer une mauvaise reponse : ");
                 String mauvais_reponse_2 =sc.nextLine();
 
+                System.out.println("\n");
                 System.out.println("Entrer une mauvaise reponse : ");
                 String mauvais_reponse_3 =sc.nextLine();
 
 
                 File chemin_dossier = new File("Questions/"+dificulte);
                 String liste_fichier[] = chemin_dossier.list();
-
                 String path=chemin_dossier+"/Question"+(liste_fichier.length+1)+".txt"; // Chemin vers le fichier à créer ou existant
 
                 File file = new File(path);
-
                 FileWriter writer = new FileWriter(file,true);
 
                 writer.write("\nQuestion_"+(liste_fichier.length+1)+" :"+question);              
@@ -80,20 +73,28 @@ public class Question {
                 writer.write("\nmauvaise_reponse_1 :"+mauvais_reponse_1);
                 writer.write("\nmauvaise_reponse_2 :"+mauvais_reponse_2);
                 writer.write("\nmauvaise_reponse_3 :"+mauvais_reponse_3);
-
                 writer.close();
+
+
+                System.out.println("\n\nLa question a bien était ajoutée ! ");
+
+                try {
+                    // Adding a 3-second delay (3000 milliseconds)
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    // Handle the exception
+                    e.printStackTrace();
+                }
+
+                System.out.print("\033[H\033[2J")   ;
+                System.out.flush();
 
                 validation = true ;
 
             }catch(Exception err){
                 System.err.println(err);
             }
-            
-
        }
-        
-
-
     }
 
     /* 
